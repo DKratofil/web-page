2a5d6ad8520856795d165320092de399e6df184d
dkratofil <dkratofil@etfos.hr>
2021-02-01 21:36:21 +0100

    web page

---
+++ includes/header.php
@@ -0,0 +1,66 @@
+<?php
+define('CSS_PATH', 'css/'); 
+?>
+<!DOCTYPE html>
+
+    
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <title> Inel Web</title>
+        
+        <link rel="icon" href="/images/logo_3__FSg_icon.ico">;
+        <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH; ?>main.css">
+
+        <link rel="stylesheet" href="/css/main.css" type="css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" ></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js" ></script>
+    </head>
+    <body>
+
+
+  
+<!---navigation Bar--->  
+     <section id="nav-bar">
+            <nav class="navbar navbar-expand-lg navbar-light ">
+                <a class="navbar-brand" href="#top"></a>
+                <?php echo "<img src='/images/logo(2).png' alt='logo'/>";
+                
+                ?>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent15" aria-controls="navbarSupportedContent15" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                 <div class="collapse navbar-collapse" id="navbarSupportedContent15">
+                  <ul class="navbar-nav ml-auto">
+                    <li class="nav-item ">
+                      <a class="nav-link" href="#top" >NASLOVNA </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/new.html" > NOVOSTI</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#services" >USLUGE</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="/one.html" >PROJEKTI</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#about"  >O NAMA</a>
+                  </li>
+                
+                <li class="nav-item">
+                <a class="nav-link" href="#suradnici" >SURADNICI</a>
+              </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#team" >NAŠ TIM</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#promo">KONTAKT</a>
+                </li>
+             </ul>
+     </div>
+    </nav>
+</section>
---
+++ one.php
@@ -0,0 +1,90 @@
+<?php require_once 'includes/header.php'; ?>
+<!----SURADNICI--------->
+<section id="suradnici">
+              <div class="container">
+                      <h1>Naši suradnici</h1>
+                      <div class="row">
+                        <div class="col-md-3 logo-pic text-center">
+                          <div class="img-box">
+                            <img src="/images/transparent_logo_zito.png" class="img-responsive" alt="zito" style="width: 200px;height:100px;">
+                              <h2>Žito d.o.o.</h2>
+                              <p>Žito grupa, vodeća prehrambena grupacija u Slavoniji i naš pouzdan partner.</p>
+                            </div>
+                          </div>
+
+                          <div class="col-md-3 logo-pic text-center">
+                            <div class="img-box">
+                              <img src="/images/transparent_logo_zito.png" class="img-responsive" alt="zito" style="width: 200px;height:100px;">
+                                <h2>Žito d.o.o.</h2>
+                                <p>Žito grupa, vodeća prehrambena grupacija u Slavoniji i naš pouzdan partner.</p>
+                              </div>
+                            </div>
+
+                            <div class="col-md-3 logo-pic text-center">
+                              <div class="img-box">
+                                <img src="/images/transparent_logo_zito.png" class="img-responsive" alt="zito" style="width: 200px;height:100px;">
+                                  <h2>Žito d.o.o.</h2>
+                                  <p>Žito grupa, vodeća prehrambena grupacija u Slavoniji i naš pouzdan partner.</p>
+                                </div>
+                              </div>
+
+                              <div class="col-md-3 logo-pic text-center">
+                                <div class="img-box">
+                                  <img src="/images/transparent_logo_zito.png" class="img-responsive" alt="zito" style="width: 200px;height:100px;">
+                                    <h2>Žito d.o.o.</h2>
+                                    <p>Žito grupa, vodeća prehrambena grupacija u Slavoniji i naš pouzdan partner.</p>
+                                  </div>
+                                </div>
+
+                                <div class="col-md-3 logo-pic text-center">
+                                  <div class="img-box">
+                                    <img src="/images/transparent_logo_zito.png" class="img-responsive" alt="zito" style="width: 200px;height:100px;">
+                                      <h2>Žito d.o.o.</h2>
+                                      <p>Žito grupa, vodeća prehrambena grupacija u Slavoniji i naš pouzdan partner.</p>
+                                    </div>
+                                  </div>
+
+                                  <div class="col-md-3 logo-pic text-center">
+                                    <div class="img-box">
+                                      <img src="/images/transparent_logo_zito.png" class="img-responsive" alt="zito" style="width: 200px;height:100px;">
+                                        <h2>Žito d.o.o.</h2>
+                                        <p>Žito grupa, vodeća prehrambena grupacija u Slavoniji i naš pouzdan partner.</p>
+                                      </div>
+                                    </div>
+
+                                    <div class="col-md-3 logo-pic text-center">
+                                      <div class="img-box">
+                                        <img src="/images/transparent_logo_zito.png" class="img-responsive" alt="zito" style="width: 200px;height:100px;">
+                                          <h2>Žito d.o.o.</h2>
+                                          <p>Žito grupa, vodeća prehrambena grupacija u Slavoniji i naš pouzdan partner.</p>
+                                        </div>
+                                      </div>
+
+                                      <div class="col-md-3 logo-pic text-center">
+                                        <div class="img-box">
+                                          <img src="/images/transparent_logo_zito.png" class="img-responsive" alt="zito" style="width: 200px;height:100px;">
+                                            <h2>Žito d.o.o.</h2>
+                                            <p>Žito grupa, vodeća prehrambena grupacija u Slavoniji i naš pouzdan partner.</p>
+                                          </div>
+                                        </div>
+                            </div>
+                      </div>
+            </section>
+
+
+ <!------FOOTER!!!!!!-->       
+ <section id="footer">
+    <div class="container text-center" >
+      <p> <a href="#"><i class="fa fa-heart"></i></a>neki potpis i slicno <a href="#"><i class="fa fa-heart"></i></a> </p>
+    </div>
+  </section>
+<!-----------------kraj footera---->
+<script src="/js/smooth-scroll.js"></script>
+  <script>
+        var scroll = new SmoothScroll('a[href*="#"]');
+</script>
+
+</body>
+</html>
+
+
